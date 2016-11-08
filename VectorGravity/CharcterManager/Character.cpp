5952d20a7ab3b98fbd890ab728c9554dab62af58
shibata0430 <shibatatetsuo0430@gmail.com>
2016-11-08 02:35:02 +0000

    Characterの誤字を修正

---
+++ VectorGravity/CharcterManager/Character.cpp
@@ -0,0 +1,39 @@
+/**
+ * @file Charcter.h
+ * @breif キャラクターの親クラスヘッダー
+ * @author shibata
+ */
+
+#include "Character.h"
+
+Character::Character() : 
+m_pGraphicsDevice(&GraphicsDevice::GetInstance())
+{
+
+}
+
+Character::~Character()
+{
+
+}
+
+void Character::Control()
+{
+
+}
+
+void Character::Draw()
+{
+
+}
+
+void Character::Attack()
+{
+
+}
+
+
+void Character::Move()
+{
+
+}
---
+++ VectorGravity/CharcterManager/Player.h
@@ -0,0 +1,21 @@
+/**
+ * @file Player.h
+ * @breif 主人公のクラスヘッダー
+ * @author shibata
+ */
+
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "Character.h"
+
+class Player : public Character
+{
+public:
+
+
+
+};
+
+
+#endif // !PLAYER_H
